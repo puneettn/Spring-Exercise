fa38aeccb1596efc799970d610eb6b410f307988
puneettn <114075135+puneettn@users.noreply.github.com>
2022-11-08 16:02:04 +0530

    updated

---
+++ RestFul_WebService_PART_2-main/src/main/java/org/ttn/sprintrest2/Sprintrest2Application.java
@@ -0,0 +1,15 @@
+package org.ttn.sprintrest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+
+public class Sprintrest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprintrest2Application.class, args);
+	}
+
+}
---
+++ RestFul_WebService_PART_2-main/src/main/java/org/ttn/sprintrest2/entity/User.java
@@ -0,0 +1,50 @@
+package org.ttn.sprintrest2.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.hateoas.RepresentationModel;
+
+public class User  extends RepresentationModel<User> {
+
+    @ApiModelProperty(notes = "User ID", example = "1", required = true)
+    private int id;
+
+    @ApiModelProperty(notes = "User Name", example = "Puneet", required = true)
+    private String name;
+    @ApiModelProperty(notes = "User Password", example = "123456", required = true)
+    private String password;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+    @JsonProperty
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+}
